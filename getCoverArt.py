217d1729d2cb396e8b97c0f9758457a541f65486
ipurton <ipurton@gmail.com>
2016-01-26 15:36:13 -0800

    Removed coverlib from search hosts
    Removed a defunct search host from script. Additional hosts still
    needed.

--- getCoverArt.py
+++ getCoverArt.py
@@ -9,9 +9,11 @@ import zipfile
 __author__ = 'ipurton'
 
 # When run in a folder containing music files, this script pulls album/artist
-# info and searches coverlib.com for images associated with the album.
+# info and searches websites for images associated with the album.
 
 # Revision Log:
+# 1/26/2015     IP
+# Removed one of the search sites; still looking for replacement.
 # 12/18/2015    IP
 # Better commenting.
 # 12/16/2015    IP
@@ -115,8 +117,10 @@ alpha_al = elimSpecials(album)
 alpha_ar = elimSpecials(artist)
 
 # Define potential search hosts
-host_urls = ["http://www.allcdcovers.com", "http://coverlib.com"]
+#host_urls = ["http://www.allcdcovers.com", "http://coverlib.com"]
+host_urls = ["http://www.allcdcovers.com"]
     # As of 11/24/2015, coverlib.com appears to be down.
+    # As of 1/26/2016, coverlib.com is not used as a search host.
 
 # Iterate through potential hosts, use the one that's up as the host
 for url in host_urls:
@@ -158,6 +162,7 @@ if s_host is host_urls[0]:
 
     # Generate count of found urls
     url_count = len(art_urls)
+    print("{} images found.".format(url_count))
 
     # If no results are found, provide an option for an alternative search
     if url_count is 0:
@@ -233,83 +238,3 @@ if s_host is host_urls[0]:
 
     print("{} images found and saved to disk.".format(ii-1))
     os.system("pause") # Windows only; use input() for other OSs
-
-## coverlib ##    
-elif s_host is host_urls[1]:
-    # Define search url using album & artist info
-    # Bizzarely, this ONLY works when formatted as "artist album".
-    # "Album artist" achieves no results!
-    url = "http://coverlib.com/search/?q={}+{}&Sektion=2".format(alpha_ar,
-                                                                 alpha_al)
-
-    # Import html from search page into an xml tree
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-
-    # coverlib's search result page returns div rows of thumbnails for full
-    # images. Each div of class=row has a div containing all thumbnails,
-    # and a unique id for that set of images.
-    # Ex: <div class="thumbnail text-muted searchitem js_href"
-    # data-href="/entry/id202809/justin-timberlake-futuresex-lovesounds">
-    # The data-href value identifies the page where the full-size images
-    # can be found.
-
-    # This finds divs of class "thumbnail ..." and returns the data-href
-    # values as a list.
-    art_urls = tree.xpath(
-        "//div[@class='thumbnail text-muted searchitem js_href']/@data-href"
-        )
-
-    if len(art_urls) is 0:
-        input("No results found for album/artist; press Enter to exit.")
-        exit()
-        
-    # Each page referred to by an art_url has a line similar to the
-    # following:
-    # Ex. <form id="EntryForm"
-    # action="/Download/zip/Justin_Timberlake-Futuresex-Lovesounds.zip"
-    # method="post">
-    # The value of the action parameter needs to be pulled for each url.
-
-    art_zips = []
-    for url in art_urls:
-        r_url = s_host + url
-        temp_r = requests.get(r_url)
-        temp_tree = html.fromstring(temp_r.content)
-        temp_zip = temp_tree.xpath("//form[@id='EntryForm']/@action")
-        art_zips.append(s_host + temp_zip[0])
-
-    # For each url in art_zips, generate a local file container and
-    # stream the zip into it. Then, extract the zip file and do some
-    # minor tidying.
-    ii = 1
-    for url in art_zips:
-        filename = "image_zip{}.zip".format(ii)
-        
-        with open(filename, "wb") as handle:
-            r = requests.get(url, stream=True)
-
-            if not r.ok:
-                print("Zip file not found at {}.".format(url))
-                break
-
-            for block in r.iter_content(1024):
-                handle.write(block)
-
-            r.close()
-
-        with zipfile.ZipFile(filename) as zip_ref:
-            zip_ref.extractall()
-
-        try:
-            os.remove(filename)
-        except PermissionError:
-            print("Zip file couldn't be removed automatically.")
-
-        if os.path.isfile("readme.txt"):
-            os.remove("readme.txt")
-
-        ii += 1
-
-    print("{} image sets found and saved to disk.".format(ii-1))
-    os.system("pause") #Windows only; use input() for other OSs
